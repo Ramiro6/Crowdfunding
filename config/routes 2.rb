6c4f6db816845e3bf7f48a53bfdf8a8aeded7156
Ramiro Ruiz <programer@Ramiros-iMac.local>
2017-03-07 10:21:56 +0100

    second

---
+++ config/routes 2.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :users, :projects
+
+  get '/', to: 'index#index'
+  get '/save', to: 'users#save'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/home', to: 'projects#home'
+  get '/config', to: 'projects#avatar'
+
+end
