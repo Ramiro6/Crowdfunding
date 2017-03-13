9b2ae2edf9ed0e06fa282651b7a5a569944cbb3b
Ramiro Ruiz <programer@Ramiros-iMac.local>
2017-03-13 16:39:07 +0100

    first

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,8 +2,13 @@ class IndexController < ApplicationController
   before_action :authorize_user, :except => [:index]
 
   def index
-    # @text = User.find(1)
-    # @entry = @text.projects
+    if current_user.nil?
+      @name = "Register/Login"
+    else
+      @name = current_user.name
+    end
+    @text = User.find(1)
+    @entry = @text.projects
   end
 
 end
